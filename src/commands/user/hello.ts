5c6751e45de27b89b728feff7f0fcf0522e42f13
tritranduc11 <tritranduc11@gamil.com>
2021-09-25 17:55:42 +0700

    update code

--- src/commands/user/hello.ts
+++ src/commands/user/hello.ts
@@ -1,12 +1,21 @@
 import { controllerInterface } from "./../../interface/index";
-import { Client, Message } from "discord.js";
+import { Client, Message, MessageEmbed } from "discord.js";
 const HelloController: controllerInterface = {
-    name: "hello",
-    category: "user",
-    aliases: ["he"],
-    run: async (client: Client<boolean>, message: Message, args: string[]) => {
-        console.log(message.channel)
-        message.channel.send(`hello ${message.author} you have the good day!`)
-    }
+  name: "hello",
+  category: "user",
+  aliases: ["he"],
+  run: async (client: Client<boolean>, message: Message, args: string[]) => {
+    const member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+    const url = member.user.displayAvatarURL();
+    const avatarEmbed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setImage(url)
+      .setURL(url)
+    message.channel.send({ embeds: [avatarEmbed] });
+    message.channel.send(`hello ${message.author} you have the good day!`);
+  },
 };
-export default HelloController
+export default HelloController;
