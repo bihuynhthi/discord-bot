a654dd107137e8358296f75435dd9989af53a207
tritranduc <tritranduc11@gmail.com>
2021-09-27 10:30:06 +0700

    change prefix to ,

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ const bot = (): void => {
       return;
     }
     if (message.author.bot) return;
-    const prefix = "!";
+    const prefix = ",";
     if (!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).trim().split(" ");
     const cmd = args.shift().toLowerCase();
